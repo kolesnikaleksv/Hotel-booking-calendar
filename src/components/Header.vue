2c201d1fa68f0a71debc81bd4477fe3e84f69f32
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-02-13 18:45:24 +0200

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+// import {ref} from 'vue'
+
+// const count = ref(0)
+
+</script>
+
+<template>
+  <div class="header">
+    <div class="header__container">
+      <div class="header__logo">
+        <img src="../assets/cerastes_cerastes.jpg">
+        Amazing
+      </div>
+      <div class="header__nav">
+        <ul>
+          <li>home</li>
+          <li>getting started</li>
+          <li>calendar</li>
+          <li>about</li>
+          <li>contacts</li>
+        </ul>
+      </div>
+      <div class="header__tools">Tools</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+  @apply h-16 shadow-md;
+  
+  &__container {
+    @apply flex flex-row justify-between items-center h-full;
+  }
+
+  &__logo {
+    @apply flex flex-row h-3/5 text-green-400 items-center text-xl;
+
+    img {
+      @apply h-full mr-2;
+    }
+  }
+  &__nav ul {
+    @apply flex flex-row;
+
+    li {
+      @apply mx-4 my-2 text-lg cursor-pointer
+      hover:shadow-lg;
+    }
+  }
+
+  &__tools {
+    @apply px-6 py-3 bg-green-500 rounded cursor-pointer text-white
+    hover:shadow-xl;
+  }
+}
+</style>
